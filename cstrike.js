a136a4e8aacfa512822b4a17d4479c2abc95089e
= <=>
2024-11-02 03:25:31 +0000

    fixes

--- cstrike.js
+++ cstrike.js
@@ -4,9 +4,9 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const path = require('path')
 
-app = express(path.resolve('/'));
+app = express();
 
-app.use(express.static())
+app.use(express.static(path.resolve('/')))
 
 app.listen(process.env.PORT, () =>{
     console.log("Alive!")
