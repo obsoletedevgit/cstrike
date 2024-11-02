7b3d8a1f57449948a5be1f1d27d0033f38bc8c6b
= <=>
2024-11-02 20:26:23 +0000

    fixes

--- cstrike.js
+++ cstrike.js
@@ -6,7 +6,9 @@ const path = require('path')
 
 app = express();
 
-app.use(express.static(path.resolve('/')))
+app.use('/', express.static(path.resolve('/')))
+app.use('/cstrike', express.static(path.resolve('/cstrike')))
+
 
 app.listen(process.env.PORT, () =>{
     console.log("Alive!")
